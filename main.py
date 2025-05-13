a415eb455860998f86d4c100965d0466b8ef6bf7
RodolfoJGonz <rodgonz0322@gmail.com>
2025-05-13 15:29:12 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,150 @@
+import sqlite3
+import csv
+
+conn = sqlite3.connect("robot.db")
+cursor = conn.cursor()
+
+# Robots Table
+exist = cursor.execute(
+    "SELECT name FROM sqlite_master WHERE type='table' AND name='Robots'"
+).fetchone()
+
+if exist is not None:
+    print("Robots Table exists")
+else:
+    print("Creating Robots Table")
+    cursor.execute("""CREATE TABLE Robots
+    (
+        Rid     INTEGER NOT NULL,
+        name    CHAR(20),
+        PRIMARY KEY (Rid)
+    );""")
+## Populate Robots table
+count = cursor.execute("SELECT COUNT(Rid) FROM Robots").fetchone()
+if count[0] == 0:
+    with open("./Data/robot.csv") as file:
+        reader = csv.DictReader(file, fieldnames=["Rid", "name"])
+        for row in reader:
+            cursor.execute(
+                "INSERT INTO Robots (Rid, name) VALUES (?,?)",
+                [int(row["Rid"]), row["name"]],
+            )
+        print("Populating Robots Table")
+        conn.commit()
+else:
+    print("Robots Table already populated")
+
+# Coordinates Table (weak entity)
+
+exist = cursor.execute(
+    "SELECT name FROM sqlite_master WHERE type='table' AND name='Coordinates'"
+).fetchone()
+
+if exist is not None:
+    print("Coordinates Table exists")
+else:
+    print("Creating Coordinates Table")
+    cursor.execute("""CREATE TABLE Coordinates
+    (
+        timestamp   INTEGER NOT NULL,
+        x_axis      FLOAT,
+        y_axis      FLOAT,
+        Rid         INTEGER NOT NULL,
+        PRIMARY KEY (Rid,timestamp),
+        FOREIGN KEY (Rid) REFERENCES Robots(Rid)
+    );""")
+
+# Populating Coordinates Table
+
+count = cursor.execute("SELECT COUNT(timestamp) FROM Coordinates").fetchone()
+if count[0] == 0:
+    for i in range(5):
+        with open(f"./Data/t{i + 1}.csv") as file:
+            row_count = 0
+            reader = csv.DictReader(file, fieldnames=["x_axis", "y_axis"])
+            for row in reader:
+                cursor.execute(
+                    "INSERT INTO Coordinates (timestamp,x_axis,y_axis,Rid) VALUES (?,?,?,?)",
+                    [row_count, float(row["x_axis"]), float(row["y_axis"]), i + 1],
+                )
+                row_count += 1
+            conn.commit()
+    print("Coordinates table populated")
+
+else:
+    print("Coordinates Table already populated")
+
+
+# Interval Table
+exist = cursor.execute(
+    "SELECT name FROM sqlite_master WHERE type='table' AND name='Intervals'"
+).fetchone()
+
+if exist is not None:
+    print("Intervals Table exists")
+else:
+    print("Creating Intervals Table")
+    cursor.execute("""CREATE TABLE Intervals
+    (
+        Iid         INTEGER NOT NULL,
+        start       INTEGER,
+        end         INTEGER,
+        event_type  CHAR(15),
+        PRIMARY KEY(Iid AUTOINCREMENT)
+    );""")
+
+# Populating Intervals Table
+count = cursor.execute("SELECT COUNT(Iid) FROM Intervals").fetchone()
+if count[0] == 0:
+    with open("./Data/interval.csv") as file:
+        reader = csv.DictReader(file, fieldnames=["start", "end", "event_type"])
+        for row in reader:
+            cursor.execute(
+                "INSERT INTO Intervals (start, end, event_type) VALUES (?,?,?)",
+                [int(row["start"]), int(row["end"]), row["event_type"]],
+            )
+
+        print("Populating Intervals Table")
+        conn.commit()
+else:
+    print("Intervals Table already populated")
+# Maybe we make a CoordinateIntervalMapping Table (BIG MAYBE)
+
+
+### TASK 3 ###
+# 1. A table consists of the names of robots and the maximal x-axis, minimum x-axis reached by this robot.
+print("\n\n3.1")
+statement = """SELECT name, MAX(x_axis), MIN(x_axis)
+FROM Robots NATURAL JOIN Coordinates
+GROUP BY name;"""
+
+cursor.execute(statement)
+
+res = cursor.execute(statement)
+
+relational_schema = [x[0] for x in res.description]
+
+print(relational_schema)
+print("-------------------------------------")
+for x in res:
+    print(x)
+
+# 2. A table consists of the names of robots and the maximal y-axis, minimum y-axis reached by this robot
+print("\n\n3.2")
+statement = """SELECT name, MAX(y_axis), MIN(y_axis)
+FROM Robots NATURAL JOIN Coordinates
+GROUP BY name;"""
+
+cursor.execute(statement)
+
+res = cursor.execute(statement)
+
+relational_schema = [x[0] for x in res.description]
+
+print(relational_schema)
+print("-------------------------------------")
+for x in res:
+    print(x)
+
+
+conn.close()
